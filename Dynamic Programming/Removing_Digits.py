3bb9b9240980c6b4be9956b7cb3d46b2eea8a51d
Grecil <grecilunadkat@gmail.com>
2025-01-02 12:50:44 +0530

    Initial commit with existing changes

--- Dynamic Programming/Removing_Digits.py
+++ Dynamic Programming/Removing_Digits.py
@@ -1,19 +1,38 @@
-from functools import lru_cache
+from types import GeneratorType
 
 
-@lru_cache(None)
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        to = f(*args, **kwargs)
+        while True:
+            if type(to) is GeneratorType:
+                stack.append(to)
+                to = next(to)
+            else:
+                stack.pop()
+                if not stack:
+                    break
+                to = stack[-1].send(to)
+        return to
+
+    return wrappedfunc
+
+
+@bootstrap
 def ways(n):
     if n == 0:
-        return 0
+        yield 0
     if arr[n - 1] != -1:
-        return arr[n - 1]
+        yield arr[n - 1]
     mn = float("inf")
     for i in str(n):
         if n - int(i) >= 0 and int(i):
-            temp = ways(n - int(i))
+            temp = yield ways(n - int(i))
             mn = min(mn, temp)
     arr[n - 1] = mn + 1
-    return mn + 1
+    yield mn + 1
 
 
 n = int(input())
