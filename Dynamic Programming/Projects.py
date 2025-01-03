b79b734632d47d7dde8984f5bb80fa643f63fc6f
Grecil <grecilunadkat@gmail.com>
2025-01-03 14:43:00 +0530

    removed redundant code

--- Dynamic Programming/Projects.py
+++ Dynamic Programming/Projects.py
@@ -2,7 +2,7 @@ from bisect import bisect_left as bsl
 
 inp = [*map(int, open(0).read().split())]
 n = inp[0]
-a, b, p = zip(*sorted(((a, b, c) for a, b, c in zip(*[iter(inp[1:])] * 3)), key=lambda x: x[1]))
+a, b, p = zip(*sorted((zip(*[iter(inp[1:])] * 3)), key=lambda x: x[1]))
 dp = [0] * (n + 1)
 for i in range(n):
     x = bsl(b, a[i])
