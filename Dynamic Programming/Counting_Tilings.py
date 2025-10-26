81561a062ab0d6bbe63bc918d8b95f69b3552094
Grecil <grecilunadkat@gmail.com>
2025-10-26 13:13:14 +0530

    Small optimization

--- Dynamic Programming/Counting_Tilings.py
+++ Dynamic Programming/Counting_Tilings.py
@@ -1,15 +1,16 @@
 mod = int(1e9 + 7)
 n, m = map(int, input().split())
-dp = [[0] * (1 << n) for _ in range(n + 1)]
+dp = [[0] * (1 << n) for i in range(n + 1)]
 dp[0][0] = 1
 for i in range(m):
     for j in range(n):
         for k in range(1 << n):
+            dp[j][k] %= mod
             if k & (1 << j):
-                dp[j + 1][k ^ (1 << j)] = (dp[j + 1][k ^ (1 << j)] + dp[j][k]) % mod
+                dp[j + 1][k ^ (1 << j)] += dp[j][k]
             else:
-                dp[j + 1][k | (1 << j)] = (dp[j + 1][k | (1 << j)] + dp[j][k]) % mod
+                dp[j + 1][k | (1 << j)] += dp[j][k]
                 if j + 1 < n and not k & (1 << (j + 1)):
-                    dp[j + 2][k] = (dp[j + 2][k] + dp[j][k]) % mod
+                    dp[j + 2][k] += dp[j][k]
     dp = [dp[-1]] + [[0] * (1 << n) for i in range(n)]
-print(dp[0][0])
+print(dp[0][0] % mod)
